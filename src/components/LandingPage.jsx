f1d5c3905abd49fcb4841e980dc691f944a50b16
david cardenas <101474305+Davidc5922@users.noreply.github.com>
2022-09-27 16:54:05 -0500

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,25 +1,56 @@
 
 
-import React from 'react'
+
 import '../styles/Landing.css'
+import React, { useState } from 'react'
+import {  useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { getDogs } from '../actions'
 
 
 import{Link} from"react-router-dom"
 
 
 export default function LandingPage(){
+    const dispatch = useDispatch()
+    const allDogs = useSelector((state) => state.dogs)
+    const imagenes = allDogs.map(e=> e.image)
+    const [Imagen,setimagen]=useState(1)
+    let contador =0;
+    useEffect(() => {
+        dispatch(getDogs())
+
+    }, [dispatch])
   
+  
+    function Img (){
+      
+            setimagen(Imagen+1)
+        if(Imagen ===172){
+            setimagen(0)
+        }
+    }
+    
+
+
     return(
-        <div className='landing'>
+        
+        <div className='landing' on>
+            
+            <img src={imagenes[Imagen]} className="fondo" />
+          
+          
+            
             <Link to="/Home">
                      
                 {/* <button hidden>/learn react/i</button> */}
-                <button className='button_Home'>bienvenido a Wiki dogs</button>
+                
+                <button className='button_Home'  onMouseOver={()=>{Img()}} >bienvenido a Wiki dogs </button>
                 </Link>
-         
+
         </div>
     )
 
 
 }
-
